b14e2651974fd0784a07567f778fcdf9105c8d6e
Ryo Suzuki <ryooopan@googlemail.com>
2014-01-08 14:50:26 +0900

    Initializing repos

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
 var app = express();
 
@@ -30,37 +30,6 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-/*
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-});
-*/
-
-var server = http.createServer(app);
-var socketio = require('socket.io');
-var io = socketio.listen(server);
-
-server.listen(app.get('port'), function(){
-  console.log("server listening on port " + app.get('port'));
-});
-
-io.sockets.on('connection', function (socket) { 
-  var address = socket.handshake.address;
-  console.log("connected from " + address.address + ":" + address.port);
-  
-  socket.on('msg', function(data){
-    console.log(data);
-    var date = new Date();
-    io.sockets.emit("msg", {date : date, message : data.message});
-  });
-  
-  socket.on('disconnect', function () {
-    console.log("disconnectted from " + address.address + ":" + address.port) 
-  });
-  
-});
-
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
 });
