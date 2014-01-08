dadecd7a23268122be16624808f476aab32c628e
Ryo Suzuki <ryooopan@googlemail.com>
2014-01-08 14:55:51 +0900

    Server setting

--- app.js
+++ app.js
@@ -30,6 +30,26 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+var server = http.createServer(app); 
+var socketio = require('socket.io'); 
+var io = socketio.listen(server); 
+
+server.listen(app.get('port'), function(){ 
+  console.log("server listening on port " + app.get('port')); 
+}); 
+
+io.sockets.on('connection', function (socket) {  
+  var address = socket.handshake.address; 
+  console.log("connected from " + address.address + ":" + address.port); 
+   
+  socket.on('msg', function(data){ 
+    console.log(data); 
+    var date = new Date(); 
+    io.sockets.emit("msg", {date : date, message : data.message}); 
+  }); 
+   
+  socket.on('disconnect', function () { 
+    console.log("disconnectted from " + address.address + ":" + address.port)  
+  }); 
+   
+}); 
